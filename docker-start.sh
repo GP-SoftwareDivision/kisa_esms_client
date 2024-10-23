96034945df285ffee2f210f8f6b8aebf748b70f5
오세은(Se Eun O) <seo@goldenplanet.co.kr>
2024-10-23 15:05:10 +0900

    [fix] 배포 스크립트 추가

--- docker-start.sh
+++ docker-start.sh
@@ -2,6 +2,9 @@ docker stop kisa-dashboard-container
 
 docker rm kisa-dashboard-container
 
+docker rmi $(docker images kisa-dashboard -q)
+
 docker build -t kisa-dashboard .
 
 docker run -d -p 80:80 --name kisa-dashboard-container kisa-dashboard
+
