fb87051981c609b0f555465e998f52d79ff6bbe8
오세은(Se Eun O) <seo@goldenplanet.co.kr>
2024-11-12 17:56:00 +0900

    [perf] ESLint 경고 수정

---
+++ src/routes/components.ts
@@ -0,0 +1,16 @@
+import { lazy } from 'react'
+
+export const MainPage = lazy(() => import('@/pages/main/DashBoardPage.tsx'))
+export const User = lazy(() => import('@/pages/manage/UserPage.tsx'))
+export const Tracking = lazy(() => import('@/pages/issue/TrackingPage.tsx'))
+export const Infringement = lazy(
+  () => import('@/pages/issue/InfringementPage.tsx')
+)
+export const DarkWeb = lazy(() => import('@/pages/retrieve/DarkWebPage.tsx'))
+export const Telegram = lazy(
+  () => import('@/pages/retrieve/TelegramDataPage.tsx')
+)
+export const Alert = lazy(() => import('@/pages/main/AlertPage.tsx'))
+export const Keyword = lazy(() => import('@/pages/manage/KeywordPage.tsx'))
+export const Ruleset = lazy(() => import('@/pages/manage/RulesetPage.tsx'))
+export const Group = lazy(() => import('@/pages/manage/GroupPage.tsx'))
